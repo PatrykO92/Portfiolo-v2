45cefd98857bad1ad3100d1e9445d860b36dcd05
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-02-21 22:14:02 +0100

    finished ContancPage component, changed styling, added message

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,6 +1,21 @@
 const ContactPage = () => {
   return (
     <div id="contact">
+      <div>
+        <span className="title">Hello there!</span>
+        <br />
+        <p>
+          <span className="message">
+            I appreciate your interest in my work.{" "}
+          </span>
+          <br />
+          Whether you have a question, a comment, or just want to say hello, I
+          am always happy to connect.
+          <br />
+          Thank you for visiting my portfolio.
+          <br />
+        </p>
+      </div>
       <form action="https://formspree.io/f/xaykzgna" method="POST">
         <h1>Reach out</h1>
         <input type="email" name="email" placeholder="Email..." />
